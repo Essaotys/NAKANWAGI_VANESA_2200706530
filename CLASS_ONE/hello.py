75843be409bf7a9c84e238a7d49f672fc5526440
essaotys <essaotys5@gmail.com>
2024-06-05 18:11:01 +0300

    First Python Assignment

---
+++ CLASS_ONE/hello.py
@@ -0,0 +1,34 @@
+# This is a comment in Python
+# Comments are lines that are ignored by the Python interpreter
+
+# Variables and Data Types
+name = "Essah"  # This is a string variable
+age = 10 # This is an integer variable
+height = 2.8  # This is a float variable
+is_student = True  # This is a boolean variable
+
+# Arithmetic Operations
+answer = age + 10  # Addition
+answer2 = age * 2  # Multiplication
+answer3 = age / 2  # Division
+remainder = age % 2  # Modulus
+
+# Conditional Statements
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+
+# 5. Lists
+numbers = [1, 2, 3,]
+names = ["Essah", "Nakanwagi", "Christine"]
+
+# 6. Functions
+def get(name):
+ print("Hello,", name)
+
+#7. Syntax
+print("Good morning, Essah")
+print('Good morning, Essah')
+print("Good' morning, Essah")
